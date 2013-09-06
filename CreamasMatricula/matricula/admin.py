66a6d5a4420362646524ee4d3e54cbda66db85bd
Hernan <hquintana@devosinc.com>
2013-09-05 21:47:11 -0500

    Modifications to admin pages for Clase, Taller and Alumno
    Add filters to Clase admin page, add list fields a filters to
    Taller and Alumno admin pages.

--- CreamasMatricula/matricula/admin.py
+++ CreamasMatricula/matricula/admin.py
@@ -3,6 +3,15 @@ from models import Usuario, Grado,Seccion, Taller, Alumno, Matricula, Nivel, Sem
 
 class ClaseAdmin(admin.ModelAdmin):
     list_display = ('grado', 'semestre', 'colegio', 'nivel')
+    list_filter = ('colegio', 'semestre')
+
+class TallerAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'semestre', 'colegio', 'estado')
+    list_filter = ('semestre', 'colegio')
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('nombreCompleto', 'colegio', 'semestre')
+    list_filter = ('semestre', 'colegio')
 
 admin.site.register(Nivel)
 admin.site.register(Semestre)
@@ -11,6 +20,6 @@ admin.site.register(Grado)
 admin.site.register(Seccion)
 admin.site.register(Colegio)
 admin.site.register(Clase, ClaseAdmin)
-admin.site.register(Taller)
-admin.site.register(Alumno)
+admin.site.register(Taller, TallerAdmin)
+admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Matricula)
